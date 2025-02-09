50186f0f663e442872d50f828ada73638808fa0b
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-02-09 21:52:54 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
   images: {
     domains: ["fakestoreapi.com"], // ✅ Allow external images
   },
-<<<<<<< HEAD
-=======
-  eslint :{
-  ignoreDuringBuilds:true,
-  },
->>>>>>> 5386cca976bc151411fdd1486945f723b9b32fd3
 };
 
 export default nextConfig;
