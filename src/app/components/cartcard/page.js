11cc0fd0b0ea44eeaca3510220e0e42de0efad2d
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-02-09 20:57:14 +0530

    Update page.js

--- src/app/components/cartcard/page.js
+++ src/app/components/cartcard/page.js
@@ -1,5 +1,6 @@
 "use client";
-import { useCart } from "../context/cartProvider";
+import { useCart } from "@/app/context/cartProvider";
+
 
 import { motion } from "framer-motion";
 import { useCallback } from "react";
