e6dbc3f89835741f058f79f08d46e49ac076a975
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-02-09 20:29:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["fakestoreapi.com"], // ✅ Allow external images
+  },
+};
 
 export default nextConfig;
